a505da3a155759b493179447ec92025f29ac031c
Michael Clayton <mike@kingslandconsulting.co.uk>
2023-01-15 23:34:34 +0000

    fixed missing namespace

--- src/FancyMouse.WindowsHotKeys/Interop/User32/User32.PostQuitMessage.cs
+++ src/FancyMouse.WindowsHotKeys/Interop/User32/User32.PostQuitMessage.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
 
 // ReSharper disable CheckNamespace
 namespace FancyMouse.WindowsHotKeys.Interop;
