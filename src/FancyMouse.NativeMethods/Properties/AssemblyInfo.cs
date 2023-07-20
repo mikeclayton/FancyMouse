8c7a653d770a5fa247d6651985bb24e15324de5f
Michael Clayton <mike@kingslandconsulting.co.uk>
2023-07-20 19:54:40 +0100

    move NativeMethods into main project

--- src/FancyMouse.NativeMethods/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-// resolve "CA1401 - P/Invoke method should not be visible"
-// by making internals available to other selected assemblies
-[assembly: InternalsVisibleTo("FancyMouse")]
-[assembly: InternalsVisibleTo("FancyMouse.UnitTests")]
-[assembly: InternalsVisibleTo("FancyMouse.WindowsHotKeys")]
--- src/FancyMouse/NativeMethods/Libraries.cs
+++ src/FancyMouse/NativeMethods/Libraries.cs
@@ -4,5 +4,6 @@ internal static class Libraries
 {
     public const string Gdi32 = "gdi32";
     public const string Kernel32 = "kernel32";
+    public const string Shell32 = "shell32";
     public const string User32 = "user32";
 }
